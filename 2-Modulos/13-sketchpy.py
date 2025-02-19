348c9aa55ec51fe23e436e4beb87dbc0e4dd3459
Wallace Mendes de Oliveira <wallace.conta2@gmail.com>
2025-02-19 15:31:39 -0300

    19.02.2025

--- 2-Modulos/13-sketchpy.py
+++ 2-Modulos/13-sketchpy.py
@@ -1,5 +1,7 @@
 from sketchpy import libary
 # object = libary.rdj()
-# object = libary.tom_holland()
-object = libary.ironman_ascii()
+object = libary.tom_holland()
+# object = libary.ironman_ascii()
 object.draw()
+
+# pyinstaller --onefile .\13-sketchpy.py
